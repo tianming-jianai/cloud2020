e2d9da45435f960c44a1eb3076a49eb5c60d27be
zsg <1724473740@qq.com>
2020-07-12 17:14:25 +0800

    ideaInjectLanguage

---
+++ java8_new_features/src/main/java/com/atguigu/springcloud/ideaInjectLanguage.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author Zhangshigang
+ * @version 0.1.0
+ * @Description
+ * @create 2020-07-12 17:05
+ * @since 0.1.0
+ **/
+public class ideaInjectLanguage {
+
+    //language=JSON
+    String json = "{\"name\": \"user\",\"passwd\": \"m123\"}";
+
+    //language=HTML
+    String html = "<body><p><span></span></p></body>";
+
+    String json1 = "";
+}
