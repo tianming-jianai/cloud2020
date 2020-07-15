60404b0d47ec9ef3bfbbfc0c814f3a4d6fec5113
zsg <1724473740@qq.com>
2020-07-15 20:59:03 +0800

    GateWay常用Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/com/atguigu/springcloud/T2.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author Zhangshigang
+ * @version 0.1.0
+ * @Description
+ * @create 2020-07-15 20:22
+ * @since 0.1.0
+ **/
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
