259b4b3eb66e35418112a8bfb56d92988d00460c
tianming-jianai <1724473740@qq.com>
2020-07-16 18:10:09 +0800

    Config配置总控中心搭建

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Zhangshigang
@@ -13,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  **/
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class, args);
