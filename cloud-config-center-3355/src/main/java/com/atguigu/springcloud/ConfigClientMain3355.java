91a3d72f95a5eb0748e053dcec95adcd4b9f0be2
tianming-jianai <1724473740@qq.com>
2020-07-16 18:15:16 +0800

    Config客户端配置与测试

---
+++ cloud-config-center-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @BelongsProject: cloud2020
+ * @BelongsPackage: com.atguigu.springcloud
+ * @Author: shiga
+ * @CreateTime: 2020-07-16 15:17
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-center-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: cloud2020
+ * @BelongsPackage: com.atguigu.springcloud.controller
+ * @Author: shiga
+ * @CreateTime: 2020-07-16 15:26
+ * @Description:
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        System.out.println("*******get ConfigInfo********");
+        return configInfo;
+    }
+}
