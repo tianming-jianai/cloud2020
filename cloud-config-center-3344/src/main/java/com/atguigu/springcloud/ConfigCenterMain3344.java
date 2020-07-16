e85fc86df41af9dbc63ecc89fb35f78409ea9f1c
zsg <1724473740@qq.com>
2020-07-16 08:30:51 +0800

    ConfigCenterMain3344

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Zhangshigang
+ * @version 0.1.0
+ * @Description
+ * @create 2020-07-15 23:02
+ * @since 0.1.0
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
