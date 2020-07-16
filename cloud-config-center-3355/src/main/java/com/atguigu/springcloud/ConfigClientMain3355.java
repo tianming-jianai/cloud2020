dfcf53e7313594b43185f3d3a319234866b0dc2c
tianming-jianai <1724473740@qq.com>
2020-07-16 18:56:13 +0800

    Config动态刷新之手动版

--- cloud-config-center-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-center-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientMain3355 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientMain3355.class, args);
